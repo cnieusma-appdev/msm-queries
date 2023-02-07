8f981bfe642c0b570a7ebe564be5ce81b30385ec
cnieusma <cnieusma@uchicago.edu>
2023-02-07 18:55:58 +0000

    movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "application", :action => "homepage" })
-
   get("/directors", { :controller => "directors", :action => "index"})
-
   get("/directors/eldest", { :controller => "directors", :action => "wisest"})
-
+  get("/directors/youngest", { :controller => "directors", :action => "young"})
   get("/directors/:an_id", { :controller => "directors", :action => "director_details"})
+
+  get("/movies", { :controller => "movies", :action => "index"})
+  get("/movies/:an_id", { :controller => "movies", :action => "movie_details"})
 
 end
